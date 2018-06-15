1591906bf1443b97d30507860adc980470a71cef
Aman-15 <amanagarwal1501@gmail.com>
2018-06-15 19:30:23 +0530

    Chapter 2 codes, pre-database handling

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func generateHTML(w http.ResponseWriter, data interface{}, fn ...string) {
+	var files []string
+
+	for _, file := range fn {
+		files = append(files, fmt.Sprintf("templates/%s.html", file))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(writer, "layout", data)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	files := []string{"templates/layout.html",
+		"templates/navbar.html",
+		"templates/index.html"}
+
+	templates := template.Must(template.ParseFiles(files...))
+
+	threads, err := data.Threads()
+	if err == nil {
+		_, err := session(w, r)
+
+		if err != nil {
+			generateHTML(w, threads, "layout", "public.navbar", "index")
+		} else {
+			generateHTML(w, threads, "layout", "private.navbar", "index")
+		}
+
+	}
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+
+	files := http.FileServer(http.Dir("/public"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/err", err)
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/logout", logout)
+	mux.HandleFunc("/signup", signup)
+	mux.HandleFunc("/signup_account", signupAccount)
+	mux.HandleFunc("/authenticate", authenticate)
+	mux.HandleFunc("/thread/new", newThread)
+	mux.HandleFunc("/thread/create", createThread)
+	mux.HandleFunc("/thread/post", postThread)
+	mux.HandleFunc("/thread/read", readThread)
+
+	server := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
